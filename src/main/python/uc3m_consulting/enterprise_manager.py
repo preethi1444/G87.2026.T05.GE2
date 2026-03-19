dd15c4f0e2f9e136093ab3bbe9a1913e35a8b0d5
preethi1444 <100589387@alumnos.uc3m.es>
2026-03-19 14:15:33 +0100

    Successful test case 14 pass

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -7,6 +7,15 @@ class EnterpriseManager:
     def __init__(self):
         pass
 
+    def register_project(self, company_cif, project_acronym, project_description, department, date, budget):
+        # --- TC14: VALIDATE DATE FORMAT ---
+        try:
+            # Python's datetime will crash if day is 32
+            req_date = datetime.strptime(date, "%d/%m/%Y")
+        except ValueError:
+            # This is the "Valid Error" your test is looking for
+            raise EnterpriseManagementException("Invalid date format")
+
     @staticmethod
     def validate_cif(cif: str):
         """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
