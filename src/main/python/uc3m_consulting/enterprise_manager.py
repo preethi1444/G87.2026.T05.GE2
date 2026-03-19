039a90711db691978ffda9745c8cb6133a855387
preethi1444 <100589387@alumnos.uc3m.es>
2026-03-19 15:05:52 +0100

    TC 23 pass

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -8,8 +8,17 @@ class EnterpriseManager:
         pass
 
     def register_project(self, company_cif, project_acronym, project_description, department, date, budget):
+        if not isinstance(budget, float):
+            raise EnterpriseManagementException("Invalid budget format")
+
         if len(company_cif) != 9:
             raise EnterpriseManagementException("Invalid CIF length")
+
+        if len(project_acronym) > 10:
+            raise EnterpriseManagementException("Invalid acronym length")
+
+        if len(project_description) < 10:
+            raise EnterpriseManagementException("Description too short")
 
         allowed_departments = ["HR", "FINANCE", "LEGAL", "LOGISTICS"]
         if department not in allowed_departments:
@@ -17,26 +26,21 @@ class EnterpriseManager:
 
         try:
             req_date = datetime.strptime(date, "%d/%m/%Y")
+            if req_date < datetime.now():
+                raise EnterpriseManagementException("Date cannot be in the past")
             if req_date.year < 2025 or req_date.year > 2027:
                 raise EnterpriseManagementException("Invalid year")
-
         except ValueError:
             raise EnterpriseManagementException("Invalid date format")
-        new_project = EnterpriseProject(company_cif, project_acronym, project_description,
-                                        department, date, budget)
+
         if budget < 0 or budget > 1000000.00:
             raise EnterpriseManagementException("Invalid budget")
 
-        if len(project_description) < 10:
-            raise EnterpriseManagementException("Description too short")
-
-        if len(project_acronym) > 10:
-            raise EnterpriseManagementException("Invalid acronym length")
-
-        if not isinstance(budget, float):
-            raise EnterpriseManagementException("Invalid budget format")
+        new_project = EnterpriseProject(company_cif, project_acronym, project_description,
+                                        department, date, budget)
 
         return new_project.project_id
+
     @staticmethod
     def validate_cif(cif: str):
         """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
