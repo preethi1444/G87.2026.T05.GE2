240b120b934fbd8b2d48017e639373703f8277bd
preethi1444 <100589387@alumnos.uc3m.es>
2026-03-19 14:47:05 +0100

    TC21 fail

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -103,5 +103,19 @@ class TestRegisterProject(unittest.TestCase):
             )
         self.assertEqual(str(context.exception), "Invalid acronym length")
 
+    def test_tc21_budget_no_decimals(self):
+        """TC21: Budget with 0 decimals"""
+        my_manager = EnterpriseManager()
+        with self.assertRaises(EnterpriseManagementException) as context:
+            my_manager.register_project(
+                company_cif="A12345674",
+                project_acronym="PROJ01",
+                project_description="Main Research Proj",
+                department="HR",
+                date="01/01/2026",
+                budget=60000
+            )
+        self.assertEqual(str(context.exception), "Invalid budget format")
+
 if __name__ == '__main__':
     unittest.main()
